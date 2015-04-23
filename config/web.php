30d434ec416cb16628ef56c6e70147ea99a30591
supergithubo <winston.los.santos@gmail.com>
2015-04-24 00:03:32 +0800

    Add authClientCollection component

--- config/web.php
+++ config/web.php
@@ -37,6 +37,21 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
+        'authClientCollection' => [
+          'class' => 'yii\authclient\Collection',
+          'clients' => [
+              'google' => [
+                  'class' => 'yii\authclient\clients\GoogleOAuth',
+                  'clientId' => 'google_client_id',
+                  'clientSecret' => 'google_client_secret',
+              ],
+              'facebook' => [
+                  'class' => 'yii\authclient\clients\Facebook',
+                  'clientId' => 'facebook_client_id',
+                  'clientSecret' => 'facebook_client_secret',
+              ],
+          ],
+        ]
     ],
     'params' => $params,
 ];
